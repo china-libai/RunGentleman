c3ceefb119de4cfc76b3154eda0a2d540a863fce
liuyj <975399405@qq.com>
2018-12-27 09:59:17 +0800

    完成首页开始动画

---
+++ js/game.js
@@ -0,0 +1,75 @@
+//防止滚动
+document.body.style.overflow='hidden';
+document.documentElement.style.overflow='hidden';
+
+
+//获取canvas
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+//定义我的人物
+var me = {
+	x: 0,
+	y: 0,
+	speed: 5, //移动速度（每帧移动像素）
+	directionChange: false, //移动方向是否切换
+	imgChangeCount: 4, //每几帧更换一次图片
+	currentZhenIndex: 0, //当前帧序号
+	imgArray: [], //人物的图片数组
+	currentImgIndex: 0 //当前图片序号
+}
+//定义其他人物
+var other = {
+	x: 0,
+	y: 0,
+	speed: 5, //移动速度（每帧移动像素）
+	imgChangeCount: 5, //每几帧更换一次图片
+	currentZhenIndex: 0, //当前帧序号
+	imgArray: [], //人物的图片数组
+	currentImgIndex: 0 //当前图片序号
+}
+
+//画出开始画面
+var drawStart = function () {
+	ctx.drawImage(bgImage, 0, 0);
+	ctx.drawImage(me.imgArray[me.currentImgIndex],me.x,me.y);
+	//更新我的人物位置
+	if(me.directionChange){
+		me.x = canvas.width/2 + 100 -16;
+		me.y -= me.speed ;	
+		if(me.y <= -32){
+			me.directionChange = false;
+		}
+	}else{
+		me.y += me.speed ;	
+		me.x = canvas.width/2-100-16;
+		if(me.y >= canvas.height){
+			me.directionChange = true;
+		}
+	}
+	//更新我的人物图片
+	me.currentZhenIndex ++;
+	if(me.currentZhenIndex>=me.imgChangeCount){
+		me.currentZhenIndex = 0;
+		me.currentImgIndex ++;
+		if(me.currentImgIndex >= me.imgArray.length){
+			me.currentImgIndex = 0;
+		}
+	}
+	ctx.drawImage(logoImage, canvas.width/2-100, canvas.height/2-200);
+	ctx.drawImage(startImage, canvas.width/2-125, canvas.height/2+50);	
+	requestAnimationFrame(drawStart);
+};
+
+//初始化
+var init = function(){
+	me.x = canvas.width/2-100;
+	me.y = -32;
+	me.imgArray = new Array();
+	me.imgArray[0] = meImage0;
+	me.imgArray[1] = meImage1;
+	drawStart();
+}
+
+init();
+
---
+++ js/loadResource.js
@@ -0,0 +1,39 @@
+//加载图片
+//背景图
+var bgImage = new Image();
+bgImage.src = "img/bg.jpg";
+//加载logo
+var logoImage = new Image();
+logoImage.src = "img/logo.png";
+
+//开始图片
+var startImage = new Image();
+startImage.src = "img/start.png";
+
+//我的人物图片0
+var meImage0 = new Image();
+meImage0.src = "img/m0.png";
+//我的人物图片1
+var meImage1 = new Image();
+meImage1.src = "img/m1.png";
+
+//其他人物图片0_0
+var otherImage0_0 = new Image();
+otherImage0_0.src = "img/other0_0.png";
+//其他人物图片0_1
+var otherImage0_1 = new Image();
+otherImage0_0.src = "img/other0_1.png";
+
+//其他人物图片1_0
+var otherImage1_0 = new Image();
+otherImage1_0.src = "img/other1_0.png";
+//其他人物图片1_1
+var otherImage1_1 = new Image();
+otherImage1_1.src = "img/other1_1.png";
+
+//其他人物图片2_0
+var otherImage2_0 = new Image();
+otherImage2_0.src = "img/other2_0.png";
+//其他人物图片2_1
+var otherImage2_1 = new Image();
+otherImage2_1.src = "img/other2_1.png";
